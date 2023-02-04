25bd5e5a11d185a611a09ff64e2aca606377b945
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-04 19:24:31 +0500

    Finall create GetRandomCats module

--- app/src/main/java/com/example/thisappwillbefunny/domain/internet/random_cats/RandomCatsRetrofitHelper.kt
+++ app/src/main/java/com/example/thisappwillbefunny/domain/internet/random_cats/RandomCatsRetrofitHelper.kt
@@ -6,12 +6,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RandomCatsRetrofitHelper {
     private const val BASE_URL = "https://aws.random.cat/"
 
-    fun getInstance(): Retrofit =
+    private fun getInstance(): Retrofit =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
 
-
+    val api = getInstance().create(GetRandomCatAPI::class.java)
 }
--- app/src/main/java/com/example/thisappwillbefunny/domain/use_cases/GetRandomCats.kt
+++ app/src/main/java/com/example/thisappwillbefunny/domain/use_cases/GetRandomCats.kt
@@ -1,12 +1,18 @@
 package com.example.thisappwillbefunny.domain.use_cases
 
+import com.example.thisappwillbefunny.domain.internet.random_cats.RandomCatsRetrofitHelper
+
 
 // https://aws.random.cat/meow
 
 class GetRandomCats {
 
-    fun execute(): String {
-        return ""
+    suspend fun execute(): String {
+        return validateEnterData()
+    }
+
+    private suspend fun validateEnterData(): String {
+        return RandomCatsRetrofitHelper.api.getCats().body()!!.file.replace("\\", "")
     }
 
 }
