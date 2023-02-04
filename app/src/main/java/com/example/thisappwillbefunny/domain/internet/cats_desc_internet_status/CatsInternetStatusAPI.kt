cfce8798ed0c05f6ab317b60cc7a2dd7031acb8c
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-04 18:51:17 +0500

    Add retrofit

---
+++ app/src/main/java/com/example/thisappwillbefunny/domain/internet/cats_desc_internet_status/CatsInternetStatusAPI.kt
@@ -0,0 +1,7 @@
+package com.example.thisappwillbefunny.domain.internet.cats_desc_internet_status
+
+interface CatsInternetStatusAPI {
+
+
+
+}
---
+++ app/src/main/java/com/example/thisappwillbefunny/domain/internet/cats_desc_internet_status/pojo/CatsInternetStatusPOJO.kt
@@ -0,0 +1,5 @@
+package com.example.thisappwillbefunny.domain.internet.cats_desc_internet_status.pojo
+
+data class CatsInternetStatusPOJO(
+    val link: String
+)
