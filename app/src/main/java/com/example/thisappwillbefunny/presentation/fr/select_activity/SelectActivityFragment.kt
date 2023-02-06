425cfc00dd63457a7ef75e22664362af160aa1af
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-06 07:17:48 +0500

    Add Current Lottie version dep

--- app/src/main/java/com/example/thisappwillbefunny/presentation/fr/select_activity/SelectActivityFragment.kt
+++ app/src/main/java/com/example/thisappwillbefunny/presentation/fr/select_activity/SelectActivityFragment.kt
@@ -1,49 +1,51 @@
 package com.example.thisappwillbefunny.presentation.fr.select_activity
 
 import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.constraintlayout.compose.ConstraintLayout
+import com.airbnb.lottie.compose.*
+import com.example.thisappwillbefunny.R
 import com.example.thisappwillbefunny.domain.internet.user_activity.pojo.ActivityPOJO
 import com.example.thisappwillbefunny.domain.use_cases.GetCurrentActivity
 import com.example.thisappwillbefunny.presentation.ui.elements.text.LargeText
 
 @Composable
-fun SelectActivityFragment() {
+fun SelectActivityFragment(viewModel: SelectActivityViewModel) {
     var content by remember { mutableStateOf<ActivityPOJO?>(null) }
-    ConstraintLayout(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        val test = createRef()
 
+    if (content != null) {
 
-        if (content != null) {
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .constrainAs(test) {
-                        top.linkTo(parent.top)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom)
-                        start.linkTo(parent.start)
-                    }
-            ) {
-                LargeText(value = content!!.activity)
-                LargeText(value = content!!.key)
-                LargeText(value = content!!.link)
-                Log.e("SelectActivityFragment",  content!!.link, )
-                LargeText(value = content!!.type)
-                LargeText(value = content!!.accessibility.toString())
-                LargeText(value = content!!.participants.toString())
-                LargeText(value = content!!.price.toString())
+        val animationSpec = remember { LottieAnimationSpec.RawRes(viewModel.getCurrentRawRes(content!!.type)) }
 
+        ConstraintLayout(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            val (topBar) = createRefs()
+
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
+                LottieAnimation(spec = )
             }
 
         }
+        Column(
 
+        ) {
+            LargeText(value = content!!.activity)
+            LargeText(value = content!!.key)
+            LargeText(value = content!!.link)
+            Log.e("SelectActivityFragment",  content!!.link, )
+            LargeText(value = content!!.type)
+            LargeText(value = content!!.accessibility.toString())
+            LargeText(value = content!!.participants.toString())
+            LargeText(value = content!!.price.toString())
+
+        }
 
     }
 
---
+++ app/src/main/java/com/example/thisappwillbefunny/presentation/fr/select_activity/SelectActivityViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.thisappwillbefunny.presentation.fr.select_activity
+
+import androidx.lifecycle.ViewModel
+import com.airbnb.lottie.compose.LottieAnimationSpec
+import com.example.thisappwillbefunny.R
+
+class SelectActivityViewModel : ViewModel() {
+
+
+    fun getCurrentRawRes(type: String): Int =
+        when(type){
+            "education" -> R.raw.reading_boy
+            "recreational" -> R.raw.relaxation
+            "social" -> R.raw.social
+            "diy" -> R.raw.diy
+            "charity" -> R.raw.charity
+            "cooking" -> R.raw.cooking
+            "relaxation" -> R.raw.relax
+            "music" -> R.raw.music
+            "busywork" -> R.raw.busywork
+            else -> -1
+        }
+}
