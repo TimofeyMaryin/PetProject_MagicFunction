0b58f91cf708f03adcc6609cef9a1dfbaa39fde1
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-12 14:06:44 +0500

    DownloadImage make as UseCase

--- app/src/main/java/com/example/thisappwillbefunny/presentation/fr/get_random_cat/GetRandomCatViewModel.kt
+++ app/src/main/java/com/example/thisappwillbefunny/presentation/fr/get_random_cat/GetRandomCatViewModel.kt
@@ -13,6 +13,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.thisappwillbefunny.domain.use_cases.DownloadImage
 import com.example.thisappwillbefunny.domain.use_cases.GetRandomCats
 import okio.IOException
 import java.io.*
@@ -28,74 +29,7 @@ class GetRandomCatViewModel(
     suspend fun getRandomCat(): String = GetRandomCats().execute()
     fun popBackStack() = navController.popBackStack()
 
-
-    fun downloadImage(url: String, context: Context){
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = android.os.Handler(Looper.getMainLooper())
-        var image: Bitmap?
-
-        executor.execute {
-            image = mLoad(url = url, context = context)
-            handler.post {
-                mSaveMediaToStorage(image, context = context)
-            }
-        }
-    }
-
-    private fun mLoad(url: String, context: Context): Bitmap? {
-        val _url: URL = mStringToURL(url)!!
-        val connection: HttpURLConnection?
-
-        try {
-            connection = _url.openConnection() as HttpURLConnection
-            connection.connect()
-
-            val inputStream: InputStream = connection.inputStream
-            val bufferedInputStream = BufferedInputStream(inputStream)
-            return BitmapFactory.decodeStream(bufferedInputStream)
-        } catch (e: IOException){
-            e.printStackTrace()
-            Log.e("downloadImage", "error ", )
-            Toast.makeText(context, "Error", Toast.LENGTH_LONG).show()
-        }
-
-        return null
-    }
-
-    private fun mStringToURL(string: String): URL? {
-        try {
-            return URL(string)
-        } catch (e: MalformedURLException){
-            e.printStackTrace()
-
-        }
-
-        return null
-    }
-
-    private fun mSaveMediaToStorage(bitmap: Bitmap?, context: Context) {
-        val filename = "${System.currentTimeMillis()}.jpg"
-        var fos: OutputStream? = null
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            context.contentResolver?.also { resolver ->
-                val contentValues = ContentValues().apply {
-                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
-                }
-                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
-                fos = imageUri?.let { resolver.openOutputStream(it) }
-            }
-        } else {
-            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-            val image = File(imagesDir, filename)
-            fos = FileOutputStream(image)
-        }
-        fos?.use {
-            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, it)
-            Toast.makeText(context , "Saved to Gallery" , Toast.LENGTH_SHORT).show()
-        }
-    }
+    fun downloadImage(url: String, context: Context) = DownloadImage.execute(url = url, context = context)
 
 
 }
