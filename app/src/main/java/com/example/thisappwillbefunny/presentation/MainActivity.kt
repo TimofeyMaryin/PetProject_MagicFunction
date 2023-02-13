355709144661ebc58f2bcb222e2900741012e881
Timfoey Maryin <nemotim4ic@gmail.com>
2023-02-13 18:15:25 +0500

    Hide status bar

--- app/src/main/java/com/example/thisappwillbefunny/presentation/MainActivity.kt
+++ app/src/main/java/com/example/thisappwillbefunny/presentation/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.thisappwillbefunny.presentation
 
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.thisappwillbefunny.domain.repository.LikeActivityRepository
 import com.example.thisappwillbefunny.presentation.navigation.AppNavController
@@ -37,8 +42,27 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        hideSystemUI()
     }
 
 
+    private fun hideSystemUI() {
+
+        //Hides the ugly action bar at the top
+        actionBar?.hide()
+
+        //Hide the status bars
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        } else {
+            window.insetsController?.apply {
+                hide(WindowInsets.Type.statusBars())
+                systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        }
+    }
 }
 
